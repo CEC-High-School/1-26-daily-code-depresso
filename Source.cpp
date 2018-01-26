d1aa26a3d0efe81340027b7a7bdd1d00ee53c64f
Deven <35495483+SaltyCracker32@users.noreply.github.com>
2018-01-26 08:25:26 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,33 @@
+#include <allegro5\allegro.h>
+#include <time.h>
+int main() {
+	srand(time(NULL));
+	ALLEGRO_DISPLAY *GameWindow = NULL; //Create a Display Window
+	al_init(); //Set up Allegro
+	GameWindow = al_create_display(640, 400); //Create Your Game Window
+	al_set_window_position(GameWindow, 200, 200); //Positions Widnow on Screen
+	al_set_window_title(GameWindow, "Ferny's Game"); //Window Title
+	while (true) {
+		int colors = rand() % 3 + 1;
+		switch (colors) {
+		case 1:
+			al_clear_to_color(al_map_rgb(250, 0, 0)); //Set it's Color
+			al_flip_display(); //Write whatever is in memory to the screen
+			system("pause");
+			break;
+		case 2:
+			al_clear_to_color(al_map_rgb(0, 250, 0)); //Set it's Color
+			al_flip_display(); //Write whatever is in memory to the screen
+			system("pause");
+			break;
+		case 3:
+			al_clear_to_color(al_map_rgb(0, 0, 250)); //Set it's Color
+			al_flip_display(); //Write whatever is in memory to the screen
+			system("pause");
+			break;
+		}
+	}
+	return 0;
+	al_destroy_display(GameWindow); //Clear Memory
 
-
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+}
